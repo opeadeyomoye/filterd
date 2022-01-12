7d37183867fd084c596a7e6a090902286d79b3e6
Ope Adeyomoye <victoradeyomoye@yahoo.co.uk>
2022-01-13 00:55:25 +0100

    Styled flash messages.

--- templates/element/flash/error.php
+++ templates/element/flash/error.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @var \App\View\AppView $this
  * @var array $params
@@ -8,4 +9,9 @@ if (!isset($params['escape']) || $params['escape'] !== false) {
     $message = h($message);
 }
 ?>
-<div class="message error" onclick="this.classList.add('hidden');"><?= $message ?></div>
+<div class="my-4 px-4 py-3 bg-red-100 rounded shadow-sm" onclick="this.classList.add('hidden');">
+    <div class="flex justify-between items-center text-sm text-red-700 font-medium">
+        <span><?= $message ?></span>
+        <button type="button" class="text-lg leading-tight">&times;</button>
+    </div>
+</div>
--- templates/element/flash/info.php
+++ templates/element/flash/info.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @var \App\View\AppView $this
  * @var array $params
@@ -8,4 +9,14 @@ if (!isset($params['escape']) || $params['escape'] !== false) {
     $message = h($message);
 }
 ?>
-<div class="message" onclick="this.classList.add('hidden');"><?= $message ?></div>
+<div class="my-4 px-4 py-3 bg-blue-100 rounded shadow-sm" onclick="this.classList.add('hidden');">
+    <div class="flex justify-between items-center text-sm text-blue-700 font-medium">
+        <div class="flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+            <span class="ml-2"><?= $message ?></span>
+        </div>
+        <button type="button" class="text-lg leading-tight">&times;</button>
+    </div>
+</div>
